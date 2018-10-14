95aa341e28f1bd605cc0472ab7c11e48c74d5ca8
Александр <forjob159@gmail.com>
2018-10-14 12:34:44 +0200

    add NProgress bar

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,14 @@
-import Nav from './Nav'
-import Link from 'next/link'
 import styled from 'styled-components'
+import Link from 'next/link'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import Nav from './Nav'
+
+Router.onRouteChangeStart = () => NProgress.start()
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
+
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -8,7 +16,6 @@ const Logo = styled.h1`
   position: relative;
   z-index: 2;
   transform: skew(-7deg);
-
   a {
     padding: 0.5rem 1rem;
     background: ${props => props.theme.red};
@@ -16,7 +23,6 @@ const Logo = styled.h1`
     text-transform: uppercase;
     text-decoration: none;
   }
-
   @media(max-width: 1300px) {
     margin:0;
     text-align: center;
@@ -35,7 +41,6 @@ const StyledHeader = styled.header`
       justify-content: center;
     }
   }
-
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;

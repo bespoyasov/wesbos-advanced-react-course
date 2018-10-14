ab71b89bee0b8da9174f773f872a87ec30cee017
Александр <forjob159@gmail.com>
2018-10-14 12:27:28 +0200

    style Nav component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
-const Nav = () => {
-  return (
-    <nav>
-      <ul>
-        <li><Link href='/sell'><a>Sell page</a></Link></li>
-        <li><Link href='/'><a>Go home!</a></Link></li>
-      </ul>
-    </nav>
-  )
-}
+const Nav = () => (
+  <NavStyles>
+    <Link href='/items'><a>Items</a></Link>
+    <Link href='/sell'><a>Sell</a></Link>
+    <Link href='/signup'><a>Signup</a></Link>
+    <Link href='/orders'><a>Orders</a></Link>
+    <Link href='/me'><a>Account</a></Link>
+  </NavStyles>
+)
 
 export default Nav

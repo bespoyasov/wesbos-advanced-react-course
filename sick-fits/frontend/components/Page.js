c9ab24d88b18515c38b1a44b037ae992853112a6
Александр <forjob159@gmail.com>
2018-10-14 12:12:06 +0200

    style Header component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -10,14 +10,14 @@ const theme = {
   red: '#ff0000',
   black: '#393939',
   grey: '#3a3a3a',
-  lightgrey: '#e1e1e1',
+  lightGrey: '#e1e1e1',
   offWhite: '#ededed',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0,0,0, .0.9)',
 }
 
 const StyledPage = styled.div`
-  background: ${props => props.theme.offWhite};
+  background: white;
   color: ${props => props.theme.black};
 `
 

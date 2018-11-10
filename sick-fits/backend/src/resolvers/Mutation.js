08d401f1a172a019dca5947bb254104e76fcf236
Александр <forjob159@gmail.com>
2018-11-10 13:30:04 +0200

    update gql providers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,18 @@
 const Mutation = {
   // TODO: check if they are logged in
   async createItem(parent, args, ctx, info) {
-    const item = await ctx.db.mutation.createItem({
+    return await ctx.db.mutation.createItem({
       data: {...args}
     }, info)
+  },
 
-    return item
+  updateItem(parent, args, ctx, info) {
+    const updates = {...args}
+    delete updates.id
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {id: args.id},
+    }, info)
   }
 }
 

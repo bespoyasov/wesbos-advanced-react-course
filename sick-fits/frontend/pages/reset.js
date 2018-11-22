6dedc6a12faa3cd5807ff0cbb688b769f918d4f2
Александр <forjob159@gmail.com>
2018-11-22 13:59:42 +0200

    add reset page

---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,10 @@
+import ResetForm from '../components/Reset'
+
+const Reset = (props) => (
+  <div>
+    <ResetForm 
+      resetToken={props.query.resetToken} />
+  </div>
+)
+
+export default Reset

a96dce648d5e7908d6a632d3e3e05aaa15332a0e
Александр <forjob159@gmail.com>
2018-11-22 12:46:13 +0200

    update links render depending on wether user is signed in or not

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,18 +3,23 @@ import NavStyles from './styles/NavStyles'
 import User from './User'
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({data: {self}}) => {
-        return !!self && <p>{self.name}</p>
-      }}
-    </User>
-    <Link href='/items'><a>Shop</a></Link>
-    <Link href='/sell'><a>Sell</a></Link>
-    <Link href='/signup'><a>Signup</a></Link>
-    <Link href='/orders'><a>Orders</a></Link>
-    <Link href='/me'><a>Account</a></Link>
-  </NavStyles>
+  <User>
+    {({data: {self}}) => (
+      <NavStyles>
+        <Link href='/items'><a>Shop</a></Link>
+        
+        {!!self && (<>
+          <Link href='/sell'><a>Sell</a></Link>
+          <Link href='/orders'><a>Orders</a></Link>
+          <Link href='/me'><a>Account</a></Link>
+        </>)}
+
+        {!self && (
+          <Link href='/signup'><a>Sign in</a></Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 )
 
 export default Nav

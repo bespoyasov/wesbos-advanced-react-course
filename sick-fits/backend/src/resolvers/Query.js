2a4a568f21215cc4e592ad5e309eab91e59f5f38
Александр <forjob159@gmail.com>
2018-11-22 12:06:51 +0200

    add resolver

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -10,6 +10,15 @@ const Query = {
 
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+
+  self(parent, args, ctx, info) {
+    const {userId} = ctx.request
+    if (!userId) return null
+    
+    return ctx.db.query.user({
+      where: {id: userId}
+    }, info)
+  },
 }
 
 module.exports = Query

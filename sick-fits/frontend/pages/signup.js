46155ee4dc0e3b40af8a69b79252950bc49d96d0
Александр <forjob159@gmail.com>
2018-11-22 11:37:22 +0200

    add signup page

---
+++ sick-fits/frontend/pages/signup.js
@@ -0,0 +1,16 @@
+import SignupForm from '../components/Signup'
+import styled from 'styled-components'
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`
+
+const Signup = () => (
+  <Columns>
+    <SignupForm />
+  </Columns>
+)
+
+export default Signup

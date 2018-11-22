62d0221be8d192b15af795a62f19f6aa6b4a80ba
Александр <forjob159@gmail.com>
2018-11-22 12:26:38 +0200

    add user render-prop usage in nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
+import User from './User'
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({data: {self}}) => {
+        return !!self && <p>{self.name}</p>
+      }}
+    </User>
     <Link href='/items'><a>Shop</a></Link>
     <Link href='/sell'><a>Sell</a></Link>
     <Link href='/signup'><a>Signup</a></Link>

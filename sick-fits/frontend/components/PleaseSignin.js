92db18f98b594f74ebe1c37516303abd84385fb3
Александр <forjob159@gmail.com>
2018-11-23 13:38:23 +0200

    add please sign in wrapper

---
+++ sick-fits/frontend/components/PleaseSignin.js
@@ -0,0 +1,19 @@
+import {Query} from 'react-apollo'
+import {CURRENT_USER_QUERY} from './User'
+import Signin from './Signin'
+
+const PleaseSignin = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({data, loading}) => {
+      if (loading) return <p>Loading...</p>
+      if (!data.self) return <div>
+        <p>Please signin</p>
+        <Signin />
+      </div>
+
+      return props.children
+    }}
+  </Query>
+)
+
+export default PleaseSignin

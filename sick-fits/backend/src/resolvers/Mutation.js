1157663afe20d7573be4fc43f9084a59c24828c9
Александр <forjob159@gmail.com>
2018-11-26 14:41:32 +0200

    add mutation resolver to add to cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -184,6 +184,39 @@ const Mutation = {
       },
     }, info)
   },
+
+  async addToCart(parent, args, ctx, info) {
+    const {userId} = ctx.request
+
+    // if they are logged in
+    if (!userId) throw new Error('You must be logged in')
+
+    // query the users current cart
+    // not cartItem cause CartItemWhereUniqueInput will get only ID
+    // and we don't know it yet
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      }
+    })
+
+    // check if the item is in cart
+    // add item or increment quantity
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 },
+      }, info)
+    }
+
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {connect: {id: userId}},
+        item: {connect: {id: args.id}},
+      }
+    }, info)
+  }
 }
 
 module.exports = Mutation

e16cb3eb8d105ac21c97d06e59edbc98586a6aef
Александр <forjob159@gmail.com>
2018-11-26 14:15:56 +0200

    add button to open cart

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,7 +1,11 @@
 import Link from 'next/link'
+import {Mutation} from 'react-apollo'
+
+import {TOGGLE_CART_MUTATION} from './Cart'
 import NavStyles from './styles/NavStyles'
 import User from './User'
 import Signout from './Signout'
+
 
 const Nav = () => (
   <User>
@@ -14,6 +18,11 @@ const Nav = () => (
           <Link href='/orders'><a>Orders</a></Link>
           <Link href='/me'><a>Account</a></Link>
           <Signout />
+
+          <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {(toggleCart) => 
+              <button onClick={toggleCart}>My cart</button>}
+          </Mutation>
         </>)}
 
         {!self && (

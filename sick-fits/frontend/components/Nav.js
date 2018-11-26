e1d006b80931e4d7f6a7e59a99d6a8db1054b85b
Александр <forjob159@gmail.com>
2018-11-26 19:58:38 +0200

    add cart count component usage

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -5,6 +5,7 @@ import {TOGGLE_CART_MUTATION} from './Cart'
 import NavStyles from './styles/NavStyles'
 import User from './User'
 import Signout from './Signout'
+import CartCount from './CartCount'
 
 
 const Nav = () => (
@@ -21,7 +22,11 @@ const Nav = () => (
 
           <Mutation mutation={TOGGLE_CART_MUTATION}>
             {(toggleCart) => 
-              <button onClick={toggleCart}>My cart</button>}
+              <button onClick={toggleCart}>
+                My cart
+                <CartCount count={self.cart.reduce((tally, cartItem) => 
+                  tally + cartItem.quantity, 0)} />
+              </button>}
           </Mutation>
         </>)}
 

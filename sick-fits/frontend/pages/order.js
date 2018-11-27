97416ae9eeec261eadd2b371f49373e75d1a1fee
Александр <forjob159@gmail.com>
2018-11-27 20:15:35 +0200

    add order page

---
+++ sick-fits/frontend/pages/order.js
@@ -0,0 +1,12 @@
+import PleaseSignin from '../components/PleaseSignin'
+import Order from '../components/Order'
+
+const OrderPage = ({query}) => (
+  <div>
+    <PleaseSignin>
+      <Order id={query.id} />
+    </PleaseSignin>
+  </div>
+)
+
+export default OrderPage

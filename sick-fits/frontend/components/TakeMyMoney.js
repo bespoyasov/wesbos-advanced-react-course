bf712d0b5e0a1b6f575f05624f7fd7c6f17b7ce6
Александр <forjob159@gmail.com>
2018-11-27 19:49:40 +0200

    fix error with empty cart

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -11,34 +11,63 @@ import calcTotalPrice from '../lib/calcTotalPrice'
 import Error from './ErrorMessage'
 import User, {CURRENT_USER_QUERY} from './User'
 
+
 const totalItems = (cart) =>
   cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)
 
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`
 
 // test card number
 // 4242 4242 4242 4242
 
 class TakeMyMoney extends React.Component {
 
-  onToken = (res) => {
-    console.log({res})
+  onToken = async (res, createOrder) => {
+    const order = await createOrder({
+      // manually call the mutation
+      variables: {token: res.id}
+    }).catch(err => 
+      alert(err.message))
+
+    console.log({order})
   }
 
   render() {
     return (
       <User>
         {({data: {self}}) => (
-          <StripeCheckout
-            description={`Order of ${totalItems(self.cart)} items`}
-            amount={calcTotalPrice(self.cart)}
-            name='Sick Fits!'
-            image={self.cart[0].item && self.cart[0].item.image}
-            stripeKey='pk_test_BSjaTiaHLkX0G5j19q4UgLcc'
-            currency='USD'
-            email={self.email}
-            token={res => this.onToken(res)}>
-            {this.props.children}
-          </StripeCheckout>
+          <Mutation 
+            refetchQueries={[{query: CURRENT_USER_QUERY}]}
+            mutation={CREATE_ORDER_MUTATION}>
+            {(createOrder, {data, loading, error}) => (
+              <StripeCheckout
+                description={`Order of ${totalItems(self.cart)} items`}
+                amount={calcTotalPrice(self.cart)}
+                name='Sick Fits!'
+                image={self.cart[0] 
+                  && self.cart[0].item 
+                  && self.cart[0].item.image}
+                stripeKey='pk_test_BSjaTiaHLkX0G5j19q4UgLcc'
+                currency='USD'
+                email={self.email}
+                token={res => this.onToken(res, createOrder)}>
+                {this.props.children}
+              </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     )

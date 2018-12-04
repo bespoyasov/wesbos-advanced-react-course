7c125e65e99d3670c63faa432b7a9f1a2dd0d95e
Александр <forjob159@gmail.com>
2018-12-04 21:37:17 +0200

    replace tests with snapshot tests

--- sick-fits/frontend/__tests__/Item.test.js
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -1,5 +1,6 @@
 import ItemComponent from '../components/Item'
 import {shallow} from 'enzyme'
+import toJSON from 'enzyme-to-json'
 
 const fakeItem = {
   id: 'test id',
@@ -11,30 +12,8 @@ const fakeItem = {
 }
 
 describe('<Item />', () => {
-  it('renders price tag properly', () => {
+  it('renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />)
-    const PriceTag = wrapper.find('PriceTag')
-    expect(PriceTag.dive().text()).toBe('$50')
-  })
-
-  it('renders title properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />)
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
-  })
-
-  it('renders the image properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />)
-    const Img = wrapper.find('img')
-    expect(Img.props().src).toBe(fakeItem.image)
-    expect(Img.props().alt).toBe(fakeItem.title)
-  })
-
-  it('renders out the buttons', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />)
-    const buttonList = wrapper.find('.buttonList')
-    expect(buttonList.children()).toHaveLength(3)
-    expect(buttonList.find('Link').exists()).toBe(true)
-    expect(buttonList.find('AddToCart').exists()).toBe(true)
-    expect(buttonList.find('DeleteItem').exists()).toBe(true)
+    expect(toJSON(wrapper)).toMatchSnapshot()
   })
 })

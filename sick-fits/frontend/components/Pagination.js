0f9aa45fa04e361a16ed00d59a7136603db540a3
Александр <forjob159@gmail.com>
2018-12-05 17:22:44 +0200

    add pagination tests

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -8,7 +8,7 @@ import Link from 'next/link'
 import Error from './ErrorMessage'
 import {prePage, perPage} from '../config'
 
-const PAGINATION_QUERY = gql`
+export const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
     itemsConnection {
       aggregate {
@@ -22,24 +22,25 @@ const Pagination = (props) => {
   return (
     <Query query={PAGINATION_QUERY}>
       {({data, loading, error}) => {
-        if (loading) return <p>Loading...</p>
+        if (loading) return <p data-test="pagination-loading">Loading...</p>
         if (error) return <Error error={error} />
         
         const {page} = props
         const count = data.itemsConnection.aggregate.count
         const pages = Math.ceil(count / perPage)
-        const label = `Page ${page} of ${pages}`
         
         return (
-          <PaginationStyles>
-            <Head><title>Sick Fits! | {label}</title></Head>
+          <PaginationStyles data-test="pagination">
+            <Head><title>Sick Fits! | Page {page} of {pages}</title></Head>
             <Link prefetch href={{
               pathname: 'items', 
               query: {page: page - 1}}}>
               <a className='prev' aria-disabled={page <= 1}>&larr; Prev</a>
             </Link>
             
-            <p>{label}</p>
+            <p>
+              Page {page} of <span data-test="pagination-total-pages">{pages}</span>
+            </p>
             <p>{count} items total</p>
 
             <Link prefetch href={{
